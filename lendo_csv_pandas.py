16a0557a81c7dfbcb4975b4fc36e4cb0d55ec08a
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2023-04-07 23:47:47 -0300

    up

--- lendo_csv_pandas.py
+++ lendo_csv_pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pylab as plt
-import numpy as np
+import seaborn as sns
 
 notas = pd.read_csv('./dados/ratings.csv')   # é um panda dataframe
 #print(notas)
@@ -14,7 +14,13 @@ notas.columns=('usuarioId', 'filmeId', 'nota', 'momento')   # muda o nome das co
 #print(notas['nota'].value_counts())   # mostra as notas e a qtde de cada nota e já ordena do mais frequente para o menos
 #print(notas['nota'].mean())   # média das notas
 #print(notas.nota)
-#print(notas.nota.plot())
-notas = plt.figure()
-# # plt.plot(np.sin(np.linspace(-np.pi, np.pi, 1001)))
-notas.show()
+
+#print(notas.nota.describe())
+
+# mostra um histograma
+# notas.nota.plot(kind='hist')
+# plt.show()
+
+# outro tipo de gráfico
+sns.boxplot(notas.nota)
+plt.show()
